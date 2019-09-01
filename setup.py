c824f92964b480b2ca66ea539b929cd44cfa4103
Henry Lunger <henry.lunger@gmail.com>
2019-09-01 12:55:28 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WeatherForecast',
+    version='2.6.3',
+    packages=[''],
+    url='https://github.com/henrybungee/weathr-forecast',
+    license='GPL v3.0',
+    author='henry-lunger',
+    author_email='henry.lunger@gmail.com',
+    description='This is an open-source current weather forecaster using OpenWeatherMaps and PyQt5 library. It is written in Python 3.'
+)
